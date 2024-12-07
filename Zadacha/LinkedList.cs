48d92b3b7634780d4e477e9eafdaf87d5ee18d21
SteliyanaMarinova <125347159+SteliyanaMarinova@users.noreply.github.com>
2024-12-07 15:09:56 +0200

    Add project files.

---
+++ Zadacha/LinkedList.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha
+{
+    public class LinkedList
+    {
+        public Node Head { get; set; }
+
+        public void Append(int value)
+        {
+            Node newNode = new Node(value);
+            if (Head == null)
+            {
+                Head = newNode;
+            }
+            else
+            {
+                Node current = Head;
+                while (current.Next != null)
+                {
+                    current = current.Next;
+                }
+                current.Next = newNode;
+            }
+        }
+
+        public void PrintList()
+        {
+            Node current = Head;
+            while (current != null)
+            {
+                Console.Write(current.Value + " ");
+                current = current.Next;
+            }
+            Console.WriteLine();
+        }
+
+        public LinkedList SplitList(string type)
+        {
+            LinkedList newList = new LinkedList();
+            Node current = Head;
+            while (current != null)
+            {
+                if (type == "even" && current.Value % 2 == 0)
+                {
+                    newList.Append(current.Value);
+                }
+                else if (type == "odd" && current.Value % 2 != 0)
+                {
+                    newList.Append(current.Value);
+                }
+                current = current.Next;
+            }
+            return newList;
+        }
+    }
+
+    public class Node
+    {
+        public int Value { get; set; }
+        public Node Next { get; set; }
+
+        public Node(int value)
+        {
+            Value = value;
+            Next = null;
+        }
+    }
+}
---
+++ Zadacha/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList originalList = new LinkedList();
+            originalList.Append(1);
+            originalList.Append(2);
+            originalList.Append(3);
+            originalList.Append(4);
+            originalList.Append(5);
+
+            LinkedList evenList = originalList.SplitList("even");
+            LinkedList oddList = originalList.SplitList("odd");
+
+            Console.WriteLine("Spisuk s chetni chisla: ");
+            evenList.PrintList();
+
+            Console.WriteLine("Spisuk s nechetni chisla: ");
+            oddList.PrintList();
+        }
+    }
+}
+
